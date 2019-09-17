ee4925af2b6e55694632b1ce5e16bece177bf0ca
Nicolas Perez <nicolagp@usc.edu>
2019-09-16 22:31:46 -0700

    presentation pipeline

--- presentation2/presentation2.py
+++ presentation2/presentation2.py
@@ -1,30 +1,22 @@
-import numpy as np
-import pandas as pd
+from sklearn.feature_selection import RFE
 from sklearn.linear_model import LinearRegression
-from sklearn.feature_selection import RFE, f_regression
+import pandas as pd
+
 
 def main():
-    data = pd.read_csv('../data/prudential-life-insurance-assessment/train.csv')
-    young = data[data['Ins_Age'] < 0.2].copy()
-    old = data[data['Ins_Age'] >= 0.2].copy()
-    data_rank = get_ranking(data)[:10]
-    old_rank = get_ranking(old)[:10]
-    young_rank = get_ranking(young)[:10]
+    original = pd.read_csv('../data/prudential-life-insurance-assessment/train.csv')
+    original = clean(original)
+    # Split into different age groups
+    young_data = original[original['Ins_Age'] < 0.2].copy()
+    old_data = original[original['Ins_Age'] >= 0.2].copy()
 
-def get_ranking(df):
-    df = clean(df)
-    X = df.drop(['Response', 'Product_Info_2', "Id"], axis=1).values
-    X2 = df.drop(['Response', 'Product_Info_2', "Id"], axis=1)
-    Y = df['Response']
-    lr = LinearRegression(normalize=True)
-    lr.fit(X, Y)
-    rfe = RFE(lr, n_features_to_select=10)
-    rfe.fit(X, Y)
+    # Get feature selection for each group
+    original_features = [i[0] for i in get_ranking(original)[:10]]
+    young_features = [i[0] for i in get_ranking(young_data)[:10]]
+    old_features = [i[0] for i in get_ranking(old_data)[:10]]
 
-    labels = list(X2.columns)
-    ranking = {labels[i]: rfe.ranking_[i] for i in range(len(labels))}
-    ranking_list = [(k, ranking[k]) for k in sorted(ranking, key=ranking.get)]
-    return ranking_list
+    # Run Random Forests
+
 
 def clean(data):
     # dropping less important columns
@@ -43,6 +35,23 @@ def clean(data):
     data['Medical_History_15'].fillna(data['Medical_History_15'].mode()[0], inplace=True)
 
     return data
+
+def get_ranking(df):
+    X = df.drop(['Response', 'Product_Info_2', "Id"], axis=1).values
+    X2 = df.drop(['Response', 'Product_Info_2', "Id"], axis=1)
+    Y = df['Response']
+    lr = LinearRegression(normalize=True)
+    lr.fit(X, Y)
+    rfe = RFE(lr, n_features_to_select=10)
+    rfe.fit(X, Y)
+
+    labels = list(X2.columns)
+    ranking = {labels[i]: rfe.ranking_[i] for i in range(len(labels))}
+    ranking_list = [(k, ranking[k]) for k in sorted(ranking, key=ranking.get)]
+    return ranking_list
+
+# def random_forests(df):
+
 
 if __name__ == '__main__':
     main()
